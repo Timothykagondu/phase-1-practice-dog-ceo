d3855a6c3dcc5eb72c0f791dbb073bd5ad8565a4
Timothy <timothy.2k21@gmail.com>
2022-08-29 20:10:52 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log('%c HI', 'color: firebrick')
+fetch ("https://dog.ceo/api/breeds/image/random/4")
+ .then (function (resp){
+  return resp.json();
+ })
+fetch ("https://dog.ceo/api/breeds/image/random/4")
+return fetch("")
+		.then((response) => response.json())
+		.then(renderBooks);
